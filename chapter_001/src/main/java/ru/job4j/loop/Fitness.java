9860657df2a949e5eed53f0da813feb16da9570e
Kot-Kot <ukrgipromez.it@gmail.com>
2019-12-14 14:57:30 +0200

    5.4. Протеиновая диета.[#205630]

--- chapter_001/src/main/java/ru/job4j/loop/Fitness.java
+++ chapter_001/src/main/java/ru/job4j/loop/Fitness.java
@@ -5,10 +5,10 @@ public class Fitness {
         //Иван за месяц увеличивает силу тяги в 3 раза за счет такой диеты,
         // а Николай - за месяц увеличивает силу тяги в 2 раза.
         int month = 0;
-        if(ivan < nik){
-            do {
-                month++;
-            }while(month * ivan * 3 < month * nik * 2);
+        while (ivan < nik){
+            month++;
+            ivan *= 3;
+            nik *= 2;
         }
 
         return month;

85006358c8326c0ad005ae6f9def72b166d24245
Kot-Kot <ukrgipromez.it@gmail.com>
2019-12-14 16:13:46 +0200

    5.8. JavaDoc. Code convention[#205638]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,27 +1,58 @@
 package ru.job4j.calculator;
-
+/**
+ * Calculator Класс для вычисления арифм операций
+ *
+ * @author Eugene Shapoval (eshapoval@yandex.ru)
+ * @version 1.0
+ * @since 01.12.19
+ */
 public class Calculator {
 
+    /**
+     * add сложение двух чисел
+     *
+     * @param first первый аргумент
+     * @param second второй аргумент
+     */
     public static void add(double first, double second) {
         double result =  first + second;
         System.out.println(first + "+" + second + " = " + result);
     }
+    /**
+     * div деление двух чисел
+     *
+     * @param first первый аргумент
+     * @param second второй аргумент
+     */
     public static void div(double first, double second) {
         double result =  first / second;
         System.out.println(first + "/" + second + " = " + result);
     }
-
+    /**
+     * multiply умножение двух чисел
+     *
+     * @param first первый аргумент
+     * @param second второй аргумент
+     */
     public static void multiply(double first, double second) {
         double result =  first * second;
         System.out.println(first + "*" + second + " = " + result);
     }
-
+    /**
+     * subtract вычитание двух чисел
+     *
+     * @param first первый аргумент
+     * @param second второй аргумент
+     */
     public static void subtract(double first, double second) {
         double result =  first - second;
         System.out.println(first + "-" + second + " = " + result);
     }
 
-
+    /**
+     * main вызов статических методов
+     * @param args аргументы
+     */
     public static void main(String[] args) {
         add(1, 1);
         div(4, 2);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Eugene Shapoval (eshapoval@yandex.ru)
+ * @version 1.1
+ * @since 0.1
+ */
+package ru.job4j.calculator;

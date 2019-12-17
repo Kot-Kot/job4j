1722318ea8d0fd466e9b2651f38ba726c8908c11
Kot-Kot <ukrgipromez.it@gmail.com>
2019-12-17 13:32:18 +0200

    6.7. Выигрышные комбинации в сокобан[#205578]

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -2,28 +2,28 @@ package ru.job4j.array;
 
 public class MatrixCheck {
     public static boolean isWin(char[][] board) {
-        boolean result = true;
-        int cellNumber = -1;
-        int rowNumber = -1;
-
-        for (int row = 0; row < board.length; row++) {
-            for (int cell = 0; cell < board.length; cell++) {
-                char sign = board[row][cell];
-                System.out.print(sign);
-                if (sign == 'X' && (cellNumber == -1 || rowNumber == -1)) {
-                    cellNumber = cell;
-                    rowNumber = row;
-                } else if (sign == 'X') {
-                    if (cellNumber != cell && rowNumber != row) {
-                        result = false;
-                        break;
+        boolean result = false;
+        int countCell = 0;
+        int countRow = 0;
+        for (int size = 0; size < board.length; size++) {
+            char sign = board[size][size];
+            if (sign == 'X') {
+                for (int size1 = 0; size1 < board.length; size1++) {
+                    if (board[size][size1] == 'X') {
+                        countCell++;
+                    }
+                    if (board[size1][size] == 'X') {
+                        countRow++;
                     }
                 }
+                if (countCell == board.length || countRow == board.length) {
+                    result = true;
+                    break;
+                }
             }
-            System.out.println();
         }
-        return result;
-    }
+     return result;
+}
 
     public static void main(String[] args) {
         char[][] hasWinVertical = {

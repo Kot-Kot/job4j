9c2e1a1206cb4e79c9bb77222f3fd88522403e50
Kot-Kot <ukrgipromez.it@gmail.com>
2019-12-25 16:50:26 +0200

    2. Перегрузка конструктора.[#205729]

--- chapter_002/src/main/java/ru/job4j/tracker/Tiger.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tiger.java
@@ -1,16 +1,16 @@
 package ru.job4j.tracker;
 
 public class Tiger extends Predator {
-    public Tiger() {
-        super();
+    public Tiger(String name) {
+        super(name);
         System.out.println("Tiger's constructor");
     }
 
     public static void main(String[] args) {
-        new Tiger();
+        new Tiger("name");
         System.out.println();
-        new Predator();
+        new Predator("name");
         System.out.println();
-        new Animal();
+        new Animal("name");
     }
 }

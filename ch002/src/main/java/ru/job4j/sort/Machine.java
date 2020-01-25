6c58c3fac13fceca6f0d2080ec9638a828104dd2
Kot-Kot <ukrgipromez.it@gmail.com>
2020-01-25 17:31:17 +0200

    Контрольные вопросы.[#205746]

---
+++ ch002/src/main/java/ru/job4j/sort/Machine.java
@@ -0,0 +1,27 @@
+package ru.job4j.sort;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Machine {
+    private final int[] COINS = {10, 5, 2, 1};
+
+    public int[] change(int money, int price) {
+        int delta = money - price;
+        int[] rsl = new int[100];
+        int size = 0;
+        for (int coin : COINS) {
+                while (delta - coin >= 0) {
+                    if (delta - coin >= 0) {
+                        rsl[size] = coin;
+                        size++;
+                        delta -= coin;
+                    } else if (delta - coin < 0) {
+                        break;
+                    }
+                }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+
+}

d773d47d2b7dd3d7dc9b90668546b549318800d8
Kot-Kot <ukrgipromez.it@gmail.com>
2020-01-30 11:32:29 +0200

    2. Сортировка User с использованием Comparator[#205654]

--- ch002/src/main/java/ru/job4j/tracker/SortItemByName.java
+++ ch002/src/main/java/ru/job4j/tracker/SortItemByName.java
@@ -1,10 +1,30 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 public class SortItemByName implements Comparator<Item> {
     @Override
     public int compare(Item i1, Item i2) {
-        return (i1.getName()).compareTo(i2.getName());
+        return i1.getName().compareTo(i2.getName());
+    }
+
+    public static void main(String[] args) {
+        List<Item> items = Arrays.asList(
+                new Item("zoo"),
+                new Item("apricot"),
+                new Item("cat"),
+                new Item("ball")
+        );
+
+
+        Collections.sort(items, new SortItemByName().reversed());
+
+        System.out.println(items);
+        for (Item item : items) {
+            System.out.println(item.getName());
+        }
     }
 }
---
+++ chapter_003/src/test/java/ru/job4j/collection/JobTest.java
@@ -0,0 +1,62 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import static org.hamcrest.Matchers.lessThan;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+public class JobTest {
+    @Test
+    public void sortByNameAndPriority() {
+        Comparator<Job> cmpNamePriority = new JobByName().thenComparing(new JobByPriority());
+        int rsl = cmpNamePriority.compare(
+                new Job("B task", 0),
+                new Job("A task", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void sortByName() {
+        List<Job> actual = new ArrayList<>();
+        actual.add(new Job("b task", 2));
+        actual.add(new Job("c task", 1));
+        actual.add(new Job("a task", 3));
+        Collections.sort(actual, new JobByName());
+        assertEquals(actual.get(0), new Job("a task", 3));
+    }
+    @Test
+    public void sortByNameReverse() {
+        List<Job> actual = new ArrayList<>();
+        actual.add(new Job("b task", 2));
+        actual.add(new Job("c task", 1));
+        actual.add(new Job("a task", 3));
+        Collections.sort(actual, new JobByName().reversed());
+        assertEquals(actual.get(0), new Job("c task", 1));
+    }
+    @Test
+    public void sortByPriority() {
+        List<Job> actual = new ArrayList<>();
+        actual.add(new Job("b task", 2));
+        actual.add(new Job("c task", 1));
+        actual.add(new Job("a task", 3));
+        Collections.sort(actual, new JobByPriority());
+        assertEquals(actual.get(0), new Job("c task", 1));
+    }
+    @Test
+    public void sortByPriorityReverse() {
+        List<Job> actual = new ArrayList<>();
+        actual.add(new Job("b task", 2));
+        actual.add(new Job("c task", 1));
+        actual.add(new Job("a task", 3));
+        Collections.sort(actual, new JobByPriority().reversed());
+        assertEquals(actual.get(0), new Job("a task", 3));
+    }
+
+}

911bb41617bbb02ec561f036dfa1d0f12b552593
Kot-Kot <ukrgipromez.it@gmail.com>
2020-01-31 13:56:19 +0200

    3. Компаратор для строк.[#205652]

--- chapter_003/src/main/java/ru/job4j/collection/StringCompare.java
+++ chapter_003/src/main/java/ru/job4j/collection/StringCompare.java
@@ -3,26 +3,27 @@ package ru.job4j.collection;
 import java.util.Comparator;
 
 public class StringCompare implements Comparator<String> {
-    @Override
-    public int compare(String left, String right) {
-        int result = -100;
-        char[] leftArr = left.toCharArray();
-        char[] rightArr = right.toCharArray();
-        int minLength = Math.min(leftArr.length, rightArr.length);
-        for (int i = 0; i < minLength; i++) {
-            if (leftArr[i] > rightArr[i]) {
-                result = 1;
-                break;
-            } else if (leftArr[i] < rightArr[i]) {
-                result = -1;
-                break;
-            } else {
-                result = 0;
+       public int compare(String left, String right) {
+        int len1 = left.length();
+        int len2 = right.length();
+        int lim = Math.min(len1, len2);
+        char[] v1 = left.toCharArray();
+        char[] v2 = right.toCharArray();
+        int k = 0;
+        while (k < lim) {
+            char c1 = v1[k];
+            char c2 = v2[k];
+            if (c1 != c2) {
+                return c1 - c2;
             }
+            k++;
         }
-        if (result == 0 && leftArr.length != rightArr.length) {
-            result = leftArr.length > rightArr.length ? 1 : -1;
-        }
-        return result;
+        return len1 - len2;
+    }
+    public static void main(String[] args) {
+        System.out.println("a".compareTo("aaa"));
+        System.out.println("a".compareTo("d"));
+        System.out.println(new StringCompare().compare("a", "aaa"));
+        System.out.println(new StringCompare().compare("a", "d"));
     }
 }

c9d725d527c1efad4fbe58b2b592d8bd48002f2d
Kot-Kot <ukrgipromez.it@gmail.com>
2020-02-12 09:10:26 +0200

    4. Преобразование List в Map.[#205544]

--- chapter_003/src/main/java/ru/job4j/stream/School.java
+++ chapter_003/src/main/java/ru/job4j/stream/School.java
@@ -3,6 +3,7 @@ package ru.job4j.stream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -13,19 +14,24 @@ public class School {
         ).collect(Collectors.toList());
         return result;
     }
+    Map<String, Integer> studentMap(List<Student> students) {
+        Map<String, Integer> result = students.stream().collect(
+                Collectors.toMap(Student::getName, Student::getScore));
+        return result;
+    }
 
     public static void main(String[] args) {
-        Student s1 = new Student(90);
-        Student s2 = new Student(80);
-        Student s3 = new Student(50);
-        Student s4 = new Student(40);
+        Student s1 = new Student("vasya", 90);
+        Student s2 = new Student("kolya", 80);
+        Student s3 = new Student("dima", 50);
+        Student s4 = new Student("misha", 40);
         List<Student> list = new ArrayList<>();
         list.add(s1);
         list.add(s2);
         list.add(s3);
         list.add(s4);
-        System.out.println(new School().collect(list, 70, 100).toString());
+        System.out.println(new School().studentMap(list).toString());
 
-
+        
     }
 }
--- chapter_003/src/main/java/ru/job4j/stream/Student.java
+++ chapter_003/src/main/java/ru/job4j/stream/Student.java
@@ -1,8 +1,22 @@
 package ru.job4j.stream;
 
+import java.util.Objects;
+
 public class Student {
-    Student(int score) {
+    private String name;
+    private int score;
+
+    public Student(String name, int score) {
+        this.name = name;
         this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public int getScore() {
@@ -13,12 +27,25 @@ public class Student {
         this.score = score;
     }
 
-    private int score;
-
     @Override
     public String toString() {
         return "Student{" +
-                "score=" + score +
+                "name='" + name + '\'' +
+                ", score=" + score +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return score == student.score &&
+                Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, score);
     }
 }
